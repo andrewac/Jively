b9c7eba5e7c258b8eb20251b9282e6ba095b78ea
nivnat <niv14nat@bu.edu>
2018-09-19 03:22:04 -0400

    added some db elements

---
+++ database/db_create_table.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 19 03:07:45 2018
+
+@author: niveditanatarajan
+"""
+#!/usr/bin/python3
+
+import pymysql
+
+# Open database connection
+db = pymysql.connect("localhost","testuser","test123","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Drop table if it already exist using execute() method.
+cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+
+# Create table as per requirement
+sql = """CREATE TABLE EMPLOYEE (
+   FIRST_NAME  CHAR(20) NOT NULL,
+   LAST_NAME  CHAR(20),
+   AGE INT,  
+   SEX CHAR(1),
+   INCOME FLOAT )"""
+
+cursor.execute(sql)
+
+# disconnect from server
+db.close()
---
+++ database/db_insert_into_table.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 19 03:12:24 2018
+
+@author: niveditanatarajan
+"""
+
+#!/usr/bin/python3
+
+import pymysql
+
+# Open database connection
+db = pymysql.connect("localhost","testuser","test123","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to INSERT a record into the database.
+sql = """INSERT INTO EMPLOYEE
+            (FIRST_NAME, LAST_NAME, AGE, SEX, INCOME)
+   VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+
+# disconnect from server
+db.close()
